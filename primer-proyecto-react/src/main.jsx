c443bb5a6bcff81e20bf5f0cfb060eaabe335b4e
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-07-11 08:31:52 +0200

    quitar el segundo componente

--- primer-proyecto-react/src/main.jsx
+++ primer-proyecto-react/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import {PrimerComponente } from './PrimerComponente'
 import './styles.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PrimerComponente />
+    
   </React.StrictMode>,
 )
