e83d26e059b911cf627d154154af7d15bfa7c706
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-07-16 10:26:55 +0200

    hello-react

---
+++ hello-react/src/Component.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+export default function Component() {
+    const [text, setText] = useState()
+    const [updated, setUpdated] = useState()
+     const textOnChange = (event) => {
+        setText(event.target.value);}
+    const buttonOnClick = () => {
+        setUpdated(text)
+    }
+    return (
+        <div>
+            <input type="text" value={text} onChange={textOnChange}/>
+            <button onClick={buttonOnClick}>Actualizar</button>
+            <p>Text input: {text}</p>
+            <p>Texto actualizado: {updated} </p>
+        </div>
+    );
+}
